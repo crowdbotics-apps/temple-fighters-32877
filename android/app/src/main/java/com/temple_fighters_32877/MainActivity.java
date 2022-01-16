e838286b73ae27ac494c749bf31254a193374e7a
Crowdbotics <team@crowdbotics.com>
2022-01-16 15:46:46 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/temple_fighters_32877/MainActivity.java
@@ -0,0 +1,15 @@
+package com.temple_fighters_32877;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "temple_fighters_32877";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "temple_fighters_32877",
+  url: "https://temple_fighters_32877.botics.co",
+  api: "https://temple_fighters_32877.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
